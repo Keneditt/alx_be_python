443aedb45b58fa9b1a7182084f45cb3fdf7e35ea
Kutlwano Mhlongo <kutlwanomakhalemele@gmail.com>
2025-07-17 11:57:40 +0200

    changed the formula

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principle = 1000
 rate = 0.05
 time = 3
-Interest = principle * rate * time
-print("The simple interest is:", Interest)
+interest = principle*rate*time
+print("The simple interest is:", interest)

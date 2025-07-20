e3b59c94bcdede023c9a82a4d42c4f050b741a67
Kutlwano Mhlongo <kutlwanomakhalemele@gmail.com>
2025-07-20 23:05:45 +0200

    changes

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,5 +1,23 @@
-first_number = int(input("Enter the first number: "))
-second_number = int(input("Enter the second number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 the_operation = input("Choose an operation (+, -, *, /): ")
-if the_operation == "*":
-    print(f"The result is: {first_number * second_number}")
+operation = input("Choose an operation (+, -, *, /): ")
+result_message = ""
+match operation:
+    case "+":
+        calculated_result = num1 + num2
+        result_message = f"The result of {num1} + {num2} is {calculated_result}."
+    case "-":
+        calculated_result = num1 - num2
+        result_message = f"The result of {num1} - {num2} is {calculated_result}."
+    case "*":
+        calculated_result = num1 * num2 
+    case "/":
+        if num2 != 0:
+            calculated_result = num1 / num2
+            result_message = f"The result of {num1} / {num2} is {calculated_result}."
+        else:
+            result_message = "cannot divide by zero."
+    case _:
+        result_message = "Error: Invalid operation chosen."
+        print(result_message)

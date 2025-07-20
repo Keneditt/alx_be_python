d76fcbbfc3de4762c1c7b384c8f74dbd41ffb0d1
Kutlwano Mhlongo <kutlwanomakhalemele@gmail.com>
2025-07-20 15:47:18 +0200

    changed if to elif on line 4

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,7 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-if weather == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat")
 if weather == "cold":
     print("Make sure you wear a warm coat and a scarf")

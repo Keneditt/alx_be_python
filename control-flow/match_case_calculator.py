21a233ba9ffef5bf2de2da7a6fd9479ba0d0d98e
Kutlwano Mhlongo <kutlwanomakhalemele@gmail.com>
2025-07-20 16:29:27 +0200

    added the code

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,5 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+the_operation = input("Choose an operation (+, -, *, /): ")
+if the_operation == "*":
+    print(f"The result is: {first_number * second_number}")

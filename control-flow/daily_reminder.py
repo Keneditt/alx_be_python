60973d259c80908be75d1f23215644086d18276c
Kutlwano Mhlongo <kutlwanomakhalemele@gmail.com>
2025-07-21 10:39:49 +0200

    re-wrote code

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,27 +1,34 @@
-task_description = input("Enter the task description: ")
+task_description = input("Enter your task: ")
 while True:
-    task_priority = input("Enter the task priority (low, medium, high): ").lower()
-    if task_priority in ["low", "medium", "high"]:
+    task_priority = input("Priority (high/medium/low): ").lower()
+    if task_priority in ["high", "medium", "low"]:
         break
     else:
-        print("Invalid priority. Please enter 'low', 'medium', or 'high'.")
+        print("Invalid priority. Please enter 'high', 'medium', or 'low'.")
 while True:
     time_bound_input = input("Is this task time-bound? (yes/no): ").lower()
-reminder = ""               
-match task_priority:   
-    case 'medium':
-        reminder = f"Reminder:{task_description} has a medium priority level."
-    case 'high':
-        reminder = f"Reminder: {task_description} has a high priority level"
-    case 'low':
-        reminder = f"Reminder: {task_description} has a low priority level."
+    if time_bound_input in ["yes", "no"]:
+        break
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
+is_time_bound = (time_bound_input) == "yes"
+reminder = ""
+match task_priority:
+    case "high":
+        reminder = f"reminder: '{task_description}' is a high priority task."
+    case "medium":
+        reminder = f"reminder: '{task_description}' is a medium priority task."
+    case "low":
+        reminder = f"reminder: '{task_description}' is a low priority task."
     case _:
-        reminder = f"Reminder: {task_description} has an unknown priority level."
-if time_bound_input:
-    reminder += "that requires immediate attention today!"
-reminder += "."
-
+        reminder = f"reminder: '{task_description}' has an unknown priority task."
+if is_time_bound:
+    reminder += " that requires immediate attention today!"
+else:
+    reminder += "."
 print(reminder)
+     
+
 
 
 
